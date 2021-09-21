881efbc6926ea5cd854394cfd66c3b6b92febfa6
Rémi Remy <meltingpot54@gmail.com>
2021-09-21 12:14:01 +0200

    affiche liste perso + pages perso Ok

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-$("#people").click(() =>
-$.get("https://swapi.dev/api/people", data=> {
-    console.log(data);
-    // console.log(data.name);
-    // $("#main").html('<div>'+data.results[1]+'</div>')
-    // for (let i in data.results) {
-    //     for(let j in data.results[i]) {
-    //         $("#main").html('<div>'+j.name+'</div>')
-    //        console.log("hello");
-        // }
-    // }
-let nb = 1
-    for (let nom of data.results) {
-        console.log(nom.name);
-        $("#main").html($("#main").html()+ '<div class="ligne">' + '<div class= "classement">'+ nb++ + " - " +'</div>' + '<div class="name">' +  nom.name +'</div>' + '</div>')
-    }
+$("#main").css("border", "none").css("box-shadow", "none")
+$(".suivant").html("")
+let suivant ='<i class="fas fa-arrow-left"></i><i class="fas fa-arrow-right"></i>'
 
-    // for (let nom of data.results) { version avec deux 0 slice (-2) permet de prendre le deux derniers ex 0027 donnera 27 et 001 => 01
-    //     console.log(nom.name);
-    //     $("#main").html($("#main").html()+ '<div class="ligne">' + '<div class= "classement">'+ ("00"+nb++).slice(-2) + " - " +'</div>' + '<div class="name">' +  nom.name +'</div>' + '</div>')
-    // }
+$("#people").click(() =>{
+    getListe("https://swapi.dev/api/people")
+ })
 
-    //  console.log(data.results[1].name);
+
+ function getListe(url){
+    $.get(url, data=> {
+        console.log(data);
+        /*
+         console.log(data.name);
+         $("#main").html('<div>'+data.results[1]+'</div>')
+         for (let i in data.results) {
+             for(let j in data.results[i]) {
+                 $("#main").html('<div>'+j.name+'</div>')
+                console.log("hello");
+             }
+         }
+         */
+        $("#main").html("")
+        let numPage = new URL(url).searchParams.get('page')||1
+        console.log(numPage);
+    let nb = (numPage-1)*10+1
+        for (let nom of data.results) {
+            console.log(nom.name);
+            $("#main").html($("#main").html()+ '<div class="ligne">' + '<div class= "classement">'+ nb++ + " - " +'</div>' + '<div class="name">' +  nom.name +'</div>' + '</div>')
+        }
+        $("#main").css("border", "gold 1.5px solid").css("box-shadow", "gold 1px 5px 5px 2px")
+        $(".suivant").html(suivant)
+        $(".fa-arrow-right").click(()=>{
+         console.log(data.next)
+         if (data.next !== null) {
+            getListe(data.next)
+         }
+        })
+        $(".fa-arrow-left").click(()=>{
+         console.log(data.previous)
+         if (data.previous !== null) {
+            getListe(data.previous)
+         }
+        }
+    )
     
-} ))
+        // for (let nom of data.results) {
+             //version avec deux 0 slice (-2) permet de prendre le deux derniers ex 0027 donnera 27 et 001 => 01
+        //     console.log(nom.name);
+        //     $("#main").html($("#main").html()+ '<div class="ligne">' + '<div class= "classement">'+ ("00"+nb++).slice(-2) + " - " +'</div>' + '<div class="name">' +  nom.name +'</div>' + '</div>')
+        // }
+    
+        //  console.log(data.results[1].name);
+        
+    })
+ }
+
 
